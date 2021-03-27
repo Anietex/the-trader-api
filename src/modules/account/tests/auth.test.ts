cdf281e73d69327f8ed4553ca1538b70fca61fda
Aniefon Umanah <aniefon.umanah@gmail.com>
2021-03-28 00:23:05 +0100

    Added basic test setup

---
+++ src/modules/account/tests/auth.test.ts
@@ -0,0 +1,11 @@
+describe('calculate', () => {
+  it('add', () => {
+    const result = 7;
+    expect(result).toBe(7);
+  });
+
+  it('substract', () => {
+    const result = 3;
+    expect(result).toBe(3);
+  });
+});
